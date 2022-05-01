9dd38ba11edde8a4f2378e16b60a6f52dc132e8c
abdellatif-elsheikh <elshikh532@gmail.com>
2022-05-01 15:46:01 +0200

    change dev env to prod env

--- udagram-frontend/src/app/app.component.ts
+++ udagram-frontend/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-import { environment } from '../environments/environment';
+import { environment } from '../environments/environment.prod';
 
 @Component({
   selector: 'app-root',
