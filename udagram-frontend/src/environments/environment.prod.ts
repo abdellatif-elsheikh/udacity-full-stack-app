c42cbb4addf467d90ca32914c2a6121dac01412e
abdellatif-elsheikh <elshikh532@gmail.com>
2022-05-01 15:55:17 +0200

    fix wrong apihost url

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://http://udacity-udagram.eba-znrwnpfi.us-east-1.elasticbeanstalk.com/api/v0",
+  apiHost: "http://udacity-udagram.eba-znrwnpfi.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
