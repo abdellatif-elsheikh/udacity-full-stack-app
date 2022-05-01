cb91619754e9cf104ec2f634df253251fe068679
abdellatif-elsheikh <elshikh532@gmail.com>
2022-05-01 08:15:34 +0200

    fixed some issues

--- udagram-frontend/src/app/app.component.ts
+++ udagram-frontend/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-import { environment } from '../environments/environment.prod';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
